9ffd871823ca1a555bcf340403fe814f9b023616
AndreySanzharov <83120731+AndreySanzharov@users.noreply.github.com>
2022-01-20 23:49:39 +0300

    flying commit

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,59 +1,82 @@
 import pygame
-import random
 
 pygame.init()
 
-MYEVENTTYPE = pygame.USEREVENT + 1
-pygame.time.set_timer(MYEVENTTYPE, 100)
+clock = pygame.time.Clock()
+fps = 60
+
+screen_w = 860
+screen_h = 760
+
+screen = pygame.display.set_mode((screen_w, screen_h))
+pygame.display.set_caption('FlappyBird :)')
+
+# стартовые значения
+
+scroll = 4
+flying = False
+game_over = False
+running = True
+# подгрузка изображений
+bg = pygame.image.load('data/bg.png')
 
 
 class Bird(pygame.sprite.Sprite):
-    def __init__(self, x, y, *group):
-        super().__init__(*group)
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("data/bird1.png")
         self.rect = self.image.get_rect()
-        self.rect.y = 10
-        self.rect.x = 100
+        self.rect.y = screen_h / 2
+        self.rect.x = screen_w / 6
+        self.grav = 0
 
-    def update(self, *args):
-        if args and args[0].type == pygame.MOUSEBUTTONDOWN:
-            self.rect = self.rect.move(0, -40)
-        if args and args[0].type == MYEVENTTYPE:
-            self.rect = self.rect.move(0, 5)
+    def update(self):
 
+        if flying == True:
+            # гравитация
+            self.grav += 1
+            if self.grav > 8:
+                self.grav = 8
+            if self.rect.bottom < 760:
+                self.rect.y += self.grav
 
-def main():
-    width = 900
-    height = 800
-    running = True
-    clock = pygame.time.Clock()
-    fps = 60
-    flying = False
-
-    score = 0
-    game_over = False
+        if game_over == False:
+            # подлет
+            if pygame.mouse.get_pressed()[0]:
+                self.grav = -10
 
 
+bird_group = pygame.sprite.Group()
 
-    size = width, height
-    screen = pygame.display.set_mode(size)
+bird = Bird(100, int(screen_h / 2))
 
-    all_sprites = pygame.sprite.Group()
+bird_group.add(bird)
 
-    Bird(100, 400, all_sprites)
+# отрисовка заднего фона
+screen.blit(bg, (0, 0))
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            all_sprites.update(event)
-        screen.fill('white')
-        all_sprites.draw(screen)
+while running:
 
-        pygame.display.flip()
-        clock.tick(fps)
-    pygame.quit()
+    clock.tick(fps)
 
+    screen.blit(bg, (0, 0))
 
-if __name__ == '__main__':
-    main()
+    bird_group.draw(screen)
+    bird_group.update()
+
+    # проверка на столкновение
+
+    if bird.rect.bottom >= 760 or bird.rect.bottom == 760:
+        game_over = True
+        flying = False
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
+
+    pygame.display.update()
+
+pygame.quit()

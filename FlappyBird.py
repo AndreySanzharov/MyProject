9bd2cebc99258d8a9e867aea2a3fb1f227abbc3a
AndreySanzharov <83120731+AndreySanzharov@users.noreply.github.com>
2022-01-20 23:09:59 +0300

    Work in lyceum 13.01

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,6 +1,10 @@
 import pygame
+import random
 
 pygame.init()
+
+MYEVENTTYPE = pygame.USEREVENT + 1
+pygame.time.set_timer(MYEVENTTYPE, 100)
 
 
 class Bird(pygame.sprite.Sprite):
@@ -11,11 +15,25 @@ class Bird(pygame.sprite.Sprite):
         self.rect.y = 10
         self.rect.x = 100
 
+    def update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN:
+            self.rect = self.rect.move(0, -40)
+        if args and args[0].type == MYEVENTTYPE:
+            self.rect = self.rect.move(0, 5)
+
 
 def main():
-    width = 700
-    height = 700
+    width = 900
+    height = 800
     running = True
+    clock = pygame.time.Clock()
+    fps = 60
+    flying = False
+
+    score = 0
+    game_over = False
+
+
 
     size = width, height
     screen = pygame.display.set_mode(size)
@@ -28,10 +46,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            all_sprites.update(event)
         screen.fill('white')
         all_sprites.draw(screen)
 
         pygame.display.flip()
+        clock.tick(fps)
     pygame.quit()
 
 

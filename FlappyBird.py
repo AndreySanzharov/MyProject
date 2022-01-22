2ff18d94137683aad7b254c43a9951dff2e0bd57
AndreySanzharov <83120731+AndreySanzharov@users.noreply.github.com>
2022-01-22 17:12:04 +0300

    restart button

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,6 +1,6 @@
 import random
-
 import pygame
+
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -10,20 +10,33 @@ screen_w = 860
 screen_h = 760
 
 screen = pygame.display.set_mode((screen_w, screen_h))
-pygame.display.set_caption('FlappyBird :)')
+pygame.display.set_caption('FlappyBird')
+
+#font = pygame.font.SysFont('Bauhaus 93', 60)
 
 # стартовые значения
-
 scroll = 6
 flying = False
 game_over = False
 rasst = 160
 pipe_chast = 1600
 last_pipe = 0
+# score = 0
 running = True
 
-# подгрузка изображений
 bg = pygame.image.load('data/bg.png')
+rest_img = pygame.image.load('data/restart.png')
+
+
+def text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
+def davay_po_nowoy_misha():
+    pipe_group.empty()
+    bird.rect.x = 100
+    bird.rect.y = 400
 
 
 class Bird(pygame.sprite.Sprite):
@@ -70,6 +83,24 @@ class Pipe(pygame.sprite.Sprite):
         self.rect.x -= scroll
 
 
+class Restart():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = x, y
+
+    def draw(self):
+        off_func = False
+        pos = pygame.mouse.get_pos()
+        # проверка условия нажатия мышки
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]:
+                off_func = True
+                davay_po_nowoy_misha()
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        return off_func
+
+
 pipe_group = pygame.sprite.Group()
 bird_group = pygame.sprite.Group()
 
@@ -79,6 +110,7 @@ bird_group.add(bird)
 
 # отрисовка заднего фона
 screen.blit(bg, (0, 0))
+button = Restart(screen_w // 2 - 50, screen_h // 2, rest_img)
 
 while running:
 
@@ -90,11 +122,16 @@ while running:
     bird_group.draw(screen)
     bird_group.update()
 
+    # проверка на столкновение
+    if pygame.sprite.groupcollide(bird_group, pipe_group, False, False):
+        game_over = True
+        flying = False
+
     if bird.rect.bottom >= 760 or bird.rect.bottom == 760:
         game_over = True
         flying = False
 
-    if flying == True and game_over == False:
+    if flying == True:
         # новые трубы
         time_now = pygame.time.get_ticks()
         if time_now - last_pipe > pipe_chast:
@@ -108,6 +145,12 @@ while running:
 
         pipe_group.update()
 
+    # Провека на конец игры и рестарт
+    if game_over:
+        if button.draw():
+            game_over = False
+            davay_po_nowoy_misha()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -117,3 +160,4 @@ while running:
     pygame.display.update()
 
 pygame.quit()
+

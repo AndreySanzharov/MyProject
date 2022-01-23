8133da18af39f27f7ad27869e77ce3e7e15e59ae
AndreySanzharov <83120731+AndreySanzharov@users.noreply.github.com>
2022-01-23 22:57:08 +0300

    add score commit

--- FlappyBird.py
+++ FlappyBird.py
@@ -12,8 +12,6 @@ screen_h = 760
 screen = pygame.display.set_mode((screen_w, screen_h))
 pygame.display.set_caption('FlappyBird')
 
-#font = pygame.font.SysFont('Bauhaus 93', 60)
-
 # стартовые значения
 scroll = 6
 flying = False
@@ -21,22 +19,24 @@ game_over = False
 rasst = 160
 pipe_chast = 1600
 last_pipe = 0
-# score = 0
+score = 0
 running = True
 
 bg = pygame.image.load('data/bg.png')
 rest_img = pygame.image.load('data/restart.png')
 
-
-def text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    screen.blit(img, (x, y))
+score_txt = pygame.font.SysFont('Bauhaus 93', 60)
 
 
 def davay_po_nowoy_misha():
     pipe_group.empty()
     bird.rect.x = 100
     bird.rect.y = 400
+
+
+def draw_text(text, score_txt, text_col, x, y):
+    img = score_txt.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 
 class Bird(pygame.sprite.Sprite):
@@ -49,18 +49,17 @@ class Bird(pygame.sprite.Sprite):
         self.rect.x = screen_w / 6
         self.grav = 0
 
+    # гравитация
     def update(self):
-
-        if flying == True:
-            # гравитация
+        if flying:
             self.grav += 1
             if self.grav > 8:
                 self.grav = 8
             if self.rect.bottom < 760:
                 self.rect.y += self.grav
 
+        # подлет
         if game_over == False:
-            # подлет
             if pygame.mouse.get_pressed()[0]:
                 self.grav = -10
 
@@ -120,28 +119,40 @@ while running:
 
     pipe_group.draw(screen)
     bird_group.draw(screen)
-    bird_group.update()
+
+    # подсчет очков
+    propusk = False
+    if len(pipe_group) > 0:
+        if bird_group.sprites()[0].rect.right > pipe_group.sprites()[0].rect.left and propusk == False:
+            propusk = True
+        if propusk:
+            if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+                score += 1
+                propusk = False
+    draw_text(str(score // 100), score_txt, "white", int(screen_w / 2), 20)
 
     # проверка на столкновение
     if pygame.sprite.groupcollide(bird_group, pipe_group, False, False):
         game_over = True
+        score = 0
         flying = False
 
-    if bird.rect.bottom >= 760 or bird.rect.bottom == 760:
+    if bird.rect.bottom >= 760 or bird.rect.top <= 0:
         game_over = True
+        score = 0
         flying = False
 
-    if flying == True:
+    if flying:
         # новые трубы
-        time_now = pygame.time.get_ticks()
-        if time_now - last_pipe > pipe_chast:
+        time = pygame.time.get_ticks()
+        if time - last_pipe > pipe_chast:
             pipe_h = random.randint(-150, 150)
             niz_pipe = Pipe(screen_w, (screen_h / 2) + pipe_h, -1)
             werh_pipe = Pipe(screen_w, (screen_h / 2) + pipe_h, 1)
 
+            last_pipe = time
             pipe_group.add(niz_pipe)
             pipe_group.add(werh_pipe)
-            last_pipe = time_now
 
         pipe_group.update()
 
@@ -158,6 +169,8 @@ while running:
             flying = True
 
     pygame.display.update()
+    bird_group.update()
 
 pygame.quit()
+
 
